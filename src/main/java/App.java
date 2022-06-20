10b5589217c6e35a9261a8ee045cb1841eba32bc
iBaq001 <backeedj@gmail.com>
2022-06-20 11:58:34 +0300

    Commit1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beenCat =
+                (Cat) applicationContext.getBean("cat");
+        Cat beenCat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean1);
+        System.out.println(beenCat == beenCat1);
     }
 }
